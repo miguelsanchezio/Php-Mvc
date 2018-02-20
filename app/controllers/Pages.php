9ea00d359a5fa91a7baf008bde37d6174940ddd6
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-20 13:57:24 -0800

    Added the Database class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,15 @@
 <?php
     class Pages extends Controller {
         public function __construct() {
-            
+            $this->postModel = $this->model('Post');
         }
 
         public function index() {
+            $posts = $this->postModel->getPosts();
+
             $data = [
-                'title' => 'Welcome'
+                'title' => 'Welcome',
+                'posts' => $posts
             ];
 
             $this->view('pages/index', $data);
@@ -20,3 +23,4 @@
             $this->view('pages/about', $data);
         }
     }
+?>
