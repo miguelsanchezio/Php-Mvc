a27117211251f43ee90515039255d0a6ce5fa878
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-20 12:00:07 -0800

    Added config file and autoload function to load libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
-    // Load libraries
-    require_once 'libraries/controller.php';
-    require_once 'libraries/core.php';
-    require_once 'libraries/database.php';
+    // Load Config
+    require_once 'config/config.php';
     
+    // Load libraries
+    // Autoload Core Libraries
+    spl_autoload_register(function($className) {
+        require_once 'libraries/' . $className . '.php';   
+    });
---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost:8888/php_mvc');
+
+    // Site Name
+    define('SITENAME', 'PHP_MVC');
+
+?>
